c45d5b4e07e9ff2c844114a88afe2ca7d6404507
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2020-02-17 17:01:58 +0100

    Go language WebSocket server experiment

---
+++ go/websocket/server.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math"
+	"net/http"
+	"time"
+
+	"gopkg.in/yaml.v2"
+
+	"github.com/gorilla/websocket"
+	"github.com/justinas/alice"
+)
+
+var err error
+var start time
+
+const yamlconfigurationfile string = "server.yaml"
+
+type Configuration struct {
+	Path string `yaml:"path"`
+	Port uint16 `yaml:"port"`
+}
+
+type Wrapper struct {
+	Record Record `json:"record"`
+}
+
+type Record struct {
+	X float64 `json:"x"`
+	Y float64 `json:"y"`
+}
+
+func logMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (configuration *Configuration) getConfiguration() *Configuration {
+	yamlfile, err := ioutil.ReadFile(yamlconfigurationfile)
+	if err != nil {
+		log.Printf("Failed to open the configuration file: %v", err)
+	}
+	err = yaml.Unmarshal(yamlfile, configuration)
+	if err != nil {
+		log.Fatalf("Failed to unmarchal the configuration file: %v", err)
+	}
+	return configuration
+}
+
+func main() {
+	start = time.Now()
+
+	var configuration Configuration
+
+	configuration.getConfiguration()
+
+	logger := alice.New(logMiddleware)
+	http.Handle(configuration.Path, logger.Then(http.HandlerFunc(wsHandler)))
+	http.ListenAndServe(fmt.Sprintf(":%d", configuration.Port), nil)
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := websocket.Upgrade(w, r, w.Header(), 1024, 1024)
+	if err != nil {
+		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
+	}
+	go echo(conn)
+}
+
+func (wrapper *Wrapper) createWrapper() *Wrapper {
+	elapsed := time.Since(start)
+	elapsedns := elapsed.Nanoseconds()
+	wrapper.Record.X = float64(elapsedns) / 1000000000
+	wrapper.Record.Y = math.Sin(wrapper.Record.X)
+}
+
+func echo(conn *websocket.Conn) {
+	var wrapper Wrapper
+	for {
+		wrapper.createWrapper()
+		if err = conn.WriteJSON(wrapper); err != nil {
+			log.Println(err)
+		}
+		time.Sleep(15 * time.Second)
+	}
+}
