38949d2fe981c5b19dd2808552a7f231ee67be2d
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2020-02-18 13:03:37 +0100

    Adding some java script web experiments

--- go/websocket/server.go
+++ go/websocket/server.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"math"
 	"net/http"
+	"strconv"
 	"time"
+
+	"github.com/gorilla/mux"
 
 	"gopkg.in/yaml.v2"
 
@@ -21,7 +25,6 @@ var start time.Time
 const yamlconfigurationfile string = "server.yaml"
 
 type Configuration struct {
-	Path  string `yaml:"path"`
 	Port  uint16 `yaml:"port"`
 	Delay uint32 `yaml:"delay"`
 }
@@ -35,6 +38,10 @@ type Record struct {
 	Y float64 `json:"y"`
 }
 
+type Records struct {
+	Records []Record `json:"records"`
+}
+
 var configuration Configuration
 
 func logMiddleware(next http.Handler) http.Handler {
@@ -42,6 +49,32 @@ func logMiddleware(next http.Handler) http.Handler {
 		log.Println(r.URL.Path)
 		next.ServeHTTP(w, r)
 	})
+}
+
+func (record *Record) createRecord(x float64) *Record {
+	record.X = x
+	record.Y = math.Sin(x)
+	return record
+}
+
+func createRecord(x float64) Record {
+	var record Record
+	record.createRecord(x)
+	return record
+}
+
+func createRecords(start float64, step float64, count int) []Record {
+	records := make([]Record, 0, count)
+	for i := 0; i < count; i++ {
+		records = append(records, createRecord(start))
+		start += step
+	}
+	return records
+}
+
+func (records *Records) createRecords(start float64, step float64, count int) *Records {
+	records.Records = createRecords(start, step, count)
+	return records
 }
 
 func (configuration *Configuration) getConfiguration() *Configuration {
@@ -56,20 +89,60 @@ func (configuration *Configuration) getConfiguration() *Configuration {
 	return configuration
 }
 
+func createRecordsPayload(start float64, step float64, count int) []byte {
+	var records Records
+	records.createRecords(start, step, count)
+	jr, err := json.Marshal(records)
+	if err != nil {
+		log.Printf("Failed to marchal records")
+	}
+	jrs := string(jr)
+	return []byte(jrs)
+}
+
+func getAll(w http.ResponseWriter, r *http.Request) {
+	payload := createRecordsPayload(0.0, 0.1, 64)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(payload)
+}
+
+func getRecords(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json")
+
+	count := -1
+	var err error
+	if val, ok := pathParams["count"]; ok {
+		count, err = strconv.Atoi(val)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"message": "need a number"}`))
+			return
+		}
+	}
+
+	payload := createRecordsPayload(0.0, 0.1, count)
+
+	w.Write(payload)
+}
+
 func main() {
 	start = time.Now()
 
 	configuration.getConfiguration()
 
-	fmt.Println("Path:", configuration.Path)
 	fmt.Println("Port:", configuration.Port)
 	fmt.Println("Delay:", configuration.Delay)
 
-	serv := fmt.Sprintf(":%d", configuration.Port)
+	router := mux.NewRouter()
+	api := router.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("", getAll).Methods(http.MethodGet)
+	api.HandleFunc("/records/{count}", getRecords).Methods(http.MethodGet)
 
 	logger := alice.New(logMiddleware)
-	http.Handle(configuration.Path, logger.Then(http.HandlerFunc(wsHandler)))
-	http.ListenAndServe(serv, nil)
+	http.Handle("/ws", logger.Then(http.HandlerFunc(wsHandler)))
+	http.ListenAndServe(fmt.Sprintf(":%d", configuration.Port), router)
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
@@ -83,9 +156,7 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 func (wrapper *Wrapper) createWrapper() *Wrapper {
 	elapsed := time.Since(start)
 	elapsedns := elapsed.Nanoseconds()
-	wrapper.Record.X = float64(elapsedns) / 1000000000
-	wrapper.Record.Y = math.Sin(wrapper.Record.X)
-	fmt.Println("X:%f,Y:%f", wrapper.Record.X, wrapper.Record.Y)
+	wrapper.Record.createRecord(float64(elapsedns) / 1000000000)
 	return wrapper
 }
 
