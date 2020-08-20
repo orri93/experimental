ec65b19da2d2c5379250b5161618c1dd20ee0e33
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2020-08-20 10:57:34 +0200

    More Spark-plug B Experimental stuff

---
+++ c/sparkplugb/sparkplugb.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+
+/*
+ *
+ */
+int main(int argc, char** argv) {
+  int result = EXIT_SUCCESS;
+
+  printf("Hello world!\n");
+
+  return result;
+}
