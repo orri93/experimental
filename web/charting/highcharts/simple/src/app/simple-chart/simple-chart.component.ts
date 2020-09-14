57fb5d8ff3e49f580170a83c4c070ed877443778
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2020-09-14 21:56:13 +0200

    Move stuff from heat-map into EFD

--- web/charting/highcharts/simple/src/app/simple-chart/simple-chart.component.ts
+++ web/charting/highcharts/simple/src/app/simple-chart/simple-chart.component.ts
@@ -1,5 +1,12 @@
 import * as Highcharts from 'highcharts';
+import * as HighchartsData from 'highcharts/modules/data';
+import darkUnicaTheme from 'highcharts/themes/dark-unica';
 import { Component, OnInit } from '@angular/core';
+
+// Changing themes using highcharts-angular
+// https://stackoverflow.com/questions/52958769/changing-themes-using-highcharts-angular
+// https://github.com/highcharts/highcharts/tree/master/js/masters/themes
+darkUnicaTheme(Highcharts);
 
 @Component({
   selector: 'app-simple-chart',
@@ -26,6 +33,16 @@ export class SimpleChartComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    HighchartsData.Highcharts.getJSON(
+      'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/usdeur.json',
+      function(jsondata) {
+        this.chartOptions.series = [{
+          type: 'area',
+          name: 'USD to EUR',
+          data: jsondata
+        }];
+      }
+    )
   }
 
 }
