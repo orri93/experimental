1718c4552c876f355bdea2b3f68bf5a666ed86c7
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2020-09-15 13:23:11 +0200

    Removing the CSV data file and adding the HTTP Client module to the Simple HighCharts experiment

--- web/charting/highcharts/simple/src/app/app.module.ts
+++ web/charting/highcharts/simple/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { HighchartsChartModule } from 'highcharts-angular';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +15,7 @@ import { SimpleChartComponent } from './simple-chart/simple-chart.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     HighchartsChartModule
   ],
   providers: [],
