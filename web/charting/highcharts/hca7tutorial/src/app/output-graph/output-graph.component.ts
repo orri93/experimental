a53b01ebfba419f713f3bb8224e18778bce33c2c
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2020-09-15 09:01:43 +0200

    Adding Angular skeleton for "the Highcharts and Angular 7" tutorial by M. Haziq see https://www.highcharts.com/blog/tutorials/highcharts-and-angular-7

---
+++ web/charting/highcharts/hca7tutorial/src/app/output-graph/output-graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-output-graph',
+  templateUrl: './output-graph.component.html',
+  styleUrls: ['./output-graph.component.css']
+})
+export class OutputGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
