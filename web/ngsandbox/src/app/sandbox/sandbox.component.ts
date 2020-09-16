263e6bf2e7f2cbfbb0b771b1732235956a4ed43c
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2020-09-16 15:43:58 +0200

    Adding data file and loading to the Angular sandbox

--- web/ngsandbox/src/app/sandbox/sandbox.component.ts
+++ web/ngsandbox/src/app/sandbox/sandbox.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sandbox',
@@ -9,15 +9,35 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SandboxComponent implements OnInit {
 
+  subscription: Subscription;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     // Sample API
-    const apiLink = '/assets/data.json';
+    const apiLink: string = '/assets/data.json';
+
+    const now: Date = new Date();
 
     this.getApiResponse(apiLink).then(
       data => {
-        console.log("Getting Data from API " + apiLink + ": " + data);
+        // Set 10 seconds interval to update data again and again
+        const source = interval(10000);
+
+        const s0 = [];
+        const s0d = data['s0'];
+        console.log("Getting Data from API " + apiLink);
+        console.log("S0: " + s0d);
+        let i: number = 0;
+        s0d.forEach(element => {
+          let ts: Date = new Date();
+          ts.setHours(now.getHours() + i++);
+          const temrow = [
+            ts, element
+          ];
+          s0.push(temrow);
+        });
+        console.log("S0: " + s0);
       },
       error => {
         console.log("Error getting API response for " + apiLink + ": " + error);
