a63aac322b8c99c703afaeecf84dab4795cc9db1
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2020-12-16 18:11:53 +0100

    Complete the bar component

--- projects/expc/src/app/bar/bar.component.ts
+++ projects/expc/src/app/bar/bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-bar',
@@ -6,7 +7,6 @@ import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
   styleUrls: ['./bar.component.css']
 })
 export class BarComponent implements OnInit {
-  @ViewChild("figure") figure: ElementRef;
 
   title = 'Bar';
   error: string;
@@ -21,17 +21,52 @@ export class BarComponent implements OnInit {
 
   private svg;
   private margin = 50;
-  private width = 750 - (this.margin * 2);
-  private height = 400 - (this.margin * 2);
+  private width = 750;
+  private height = 400;
 
   constructor() { }
 
   private createSvg(): void {
-    var ne = this.figure.nativeElement;
+    this.svg = d3.select("figure#bar-figure")
+      .append('svg')
+      .attr('width', this.width)
+      .attr('height', this.height)
+      .append('g')
+      .attr('transform', 'translate(' + this.margin + ',' + this.margin + ')');
   }
 
   private drawBars(data: any[]): void {
-    
+    // Add X axis
+    const x = d3.scaleBand()
+    .range([0, this.width])
+    .domain(data.map(d => d.Framework))
+    .padding(0.2);
+
+    this.svg.append("g")
+    .attr("transform", "translate(0," + this.height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
+
+    // Add Y axis
+    const y = d3.scaleLinear()
+    .domain([0, 200000])
+    .range([this.height, 0]);
+
+    this.svg.append("g")
+    .call(d3.axisLeft(y));
+
+    // Create and fill the bars
+    this.svg.selectAll("bars")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", d => x(d.Framework))
+    .attr("y", d => y(d.Stars))
+    .attr("width", x.bandwidth())
+    .attr("height", (d) => this.height - y(d.Stars))
+    .attr("fill", "#d04a35");
   }
 
   ngOnInit(): void {
