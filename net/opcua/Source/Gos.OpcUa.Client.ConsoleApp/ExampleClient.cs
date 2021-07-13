dd41895c71d98dcfe4888b5a9a8f1dd5002c8185
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2021-07-13 13:07:52 +0200

    Add .Net OPC UA LibUA Client Experiment

---
+++ net/opcua/Source/Gos.OpcUa.Client.ConsoleApp/ExampleClient.cs
@@ -0,0 +1,112 @@
+﻿using LibUA.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gos.OpcUa.Client.ConsoleApp
+{
+    public class ExampleClient : LibUA.Client
+    {
+        private const string CertificateFileName = "Client.der";
+        private const string PrivateKeyFileName = "Client.pem";
+        private const string DefaultName = "CN=Client certificate;OU=Demo organization";
+        private const string DefaultUri = "urn:DemoApplication";
+
+        private X509Certificate2 applicationCertificate = null;
+        private RSACryptoServiceProvider applicationPrivateKey = null;
+
+        public override X509Certificate2 ApplicationCertificate
+        {
+            get { return applicationCertificate; }
+        }
+
+        public override RSACryptoServiceProvider ApplicationPrivateKey
+        {
+            get { return applicationPrivateKey; }
+        }
+
+        public ExampleClient(string Target, int Port, int Timeout) : base(Target, Port, Timeout)
+        {
+            LoadCertificateAndPrivateKey();
+        }
+
+        public override void NotifyDataChangeNotifications(uint subscrId, uint[] clientHandles, DataValue[] notifications)
+        {
+            for (int i = 0; i < clientHandles.Length; i++)
+            {
+                Console.WriteLine(
+                    "subscrId {0} handle {1}: {2}",
+                    subscrId,
+                    clientHandles[i],
+                    notifications[i].Value.ToString());
+            }
+        }
+
+        public override void NotifyEventNotifications(uint subscrId, uint[] clientHandles, object[][] notifications)
+        {
+            for (int i = 0; i < clientHandles.Length; i++)
+            {
+                Console.WriteLine(
+                    "subscrId {0} handle {1}: {2}",
+                    subscrId,
+                    clientHandles[i],
+                    string.Join(",", notifications[i]));
+            }
+        }
+
+        private void LoadCertificateAndPrivateKey()
+        {
+            try
+            {
+                // Try to load existing (public key) and associated private key
+                this.applicationCertificate = new X509Certificate2(CertificateFileName);
+                this.applicationPrivateKey = new RSACryptoServiceProvider();
+                string clientKeyText = File.ReadAllText(PrivateKeyFileName);
+                RSAParameters rsaPrivateParameters = LibUA.UASecurity.ImportRSAPrivateKey(clientKeyText);
+                this.applicationPrivateKey.ImportParameters(rsaPrivateParameters);
+            }
+            catch
+            {
+                // Make a new certificate (public key) and associated private key
+                CreateCertificateAndPrivateKey();
+            }
+        }
+
+        private void CreateCertificateAndPrivateKey()
+        {
+            X500DistinguishedName name = new X500DistinguishedName(
+                DefaultName,
+                X500DistinguishedNameFlags.UseSemicolons);
+            SubjectAlternativeNameBuilder builder = new SubjectAlternativeNameBuilder();
+            builder.AddUri(new Uri(DefaultUri));
+            using (RSA rsa = RSA.Create(2048))
+            {
+                CertificateRequest request = new CertificateRequest(
+                    name,
+                    rsa,
+                    HashAlgorithmName.SHA256,
+                    RSASignaturePadding.Pkcs1);
+                request.CertificateExtensions.Add(builder.Build());
+                X509Certificate2 certificate = request.CreateSelfSigned(
+                    new DateTimeOffset(DateTime.UtcNow.AddDays(-1)),
+                    new DateTimeOffset(DateTime.UtcNow.AddDays(3650)));
+                this.applicationCertificate = new X509Certificate2(
+                    certificate.Export(X509ContentType.Pfx, ""),
+                    "",
+                    X509KeyStorageFlags.DefaultKeySet);
+                RSAParameters rsaPrivateParameters = rsa.ExportParameters(true);
+                File.WriteAllText(
+                    CertificateFileName,
+                    LibUA.UASecurity.ExportPEM(this.applicationCertificate));
+                File.WriteAllText(
+                    PrivateKeyFileName,
+                    LibUA.UASecurity.ExportRSAPrivateKey(rsaPrivateParameters));
+            }
+        }
+    }
+}
---
+++ net/opcua/Source/Gos.OpcUa.Client.ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using LibUA.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gos.OpcUa.Client.ConsoleApp
+{
+    class Program
+    {
+        private const string ApplicationUri = "urn:DemoApplication";
+        private const string ProductUri = "uri:DemoApplication";
+        private const string ApplicationName = "UA SDK client";
+        private const string LocaleId = "en";
+
+        private static string[] LocaleIds = new[] { LocaleId };
+
+        static void Main(string[] args)
+        {
+            ApplicationDescription applicationDescription = new ApplicationDescription(
+                ApplicationUri,
+                ProductUri,
+                new LocalizedText(ApplicationName),
+                ApplicationType.Client,
+                null, null, null);
+
+            ApplicationDescription[] applicationDescriptions = null;
+            EndpointDescription[] endpointDescriptions = null;
+
+            ExampleClient client = new ExampleClient("127.0.0.1", 7718, 1000);
+            client.Connect();
+            client.OpenSecureChannel(MessageSecurityMode.None, SecurityPolicy.None, null);
+            client.FindServers(out applicationDescriptions, LocaleIds);
+            client.GetEndpoints(out endpointDescriptions, LocaleIds);
+            client.Disconnect();
+
+            // Check matching message security mode and security policy too
+            // Lazy way to find server certificate is just grab any endpoint with one
+            byte[] serverCertificate = endpointDescriptions
+                .First(e => e.ServerCertificate != null && e.ServerCertificate.Length > 0)
+                .ServerCertificate;
+            string usernamePolicyDescription = endpointDescriptions
+                .First(e => e.UserIdentityTokens.Any(t => t.TokenType == UserTokenType.UserName))
+                .UserIdentityTokens.First(t => t.TokenType == UserTokenType.UserName)
+                .PolicyId;
+
+            client.Dispose();
+        }
+    }
+}
